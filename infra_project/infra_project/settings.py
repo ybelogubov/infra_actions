8537acce88672bbc2c4a4439914976b79b4eb6a7
Yurii <belogubov.y@gmail.com>
2021-07-05 00:45:22 +0300

    +docker and deploy

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [84.201.129.84]
+ALLOWED_HOSTS = [
+    '84.201.129.84',
+    ]
 
 
 # Application definition
